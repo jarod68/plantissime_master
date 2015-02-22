e2c7a3bda4aab829fce76a881b8d6a9f2fd4be4b
Efynox <efynox@live.fr>
2015-02-23 00:34:00 +0100

    Implement plants list, plant creation Add required packages Move views from views/ to res/views/ Add .htaccess file for apache2 server

--- webapp/src/Plantissime/AppControllerProvider.php
+++ webapp/src/Plantissime/AppControllerProvider.php
@@ -6,6 +6,8 @@ use Silex\Application;
 use Silex\ControllerProviderInterface;
 use Silex\ControllerCollection;
 use Silex\Application\TwigTrait;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class AppControllerProvider implements ControllerProviderInterface
 {
@@ -14,19 +16,61 @@ class AppControllerProvider implements ControllerProviderInterface
         // creates a new controller based on the default route
         $controllers = $app['controllers_factory'];
 
+
     	// 
-        $controllers->get('/', function (Application $app) {
-            return $app['twig']->render('hello.twig', array(
-                    'name' => 'Fabien'
-                ));
-        });
-
-        $controllers->get('/{name}', function (Application $app, $name) {
-            return $app->render('hello.twig', ['name' => $name]);
-        });
-
-
+        $controllers->get('/', 'Plantissime\AppControllerProvider::home');
+        $controllers->get('/plant', 'Plantissime\AppControllerProvider::plantList');
+        $controllers->match('/plant/create', 'Plantissime\AppControllerProvider::plantCreate');
+        $controllers->get('/plant/{id}', 'Plantissime\AppControllerProvider::plantDetail');
 
         return $controllers;
     }
+
+    public function home(Application $app)
+    {
+        return $app->render('home.twig');
+    }
+
+    public function plantList(Application $app)
+    {
+        $plants = $app['db']->fetchAll('SELECT * FROM Plants');
+        return $app['twig']->render('plantList.twig', array('plants' => $plants));
+    }
+
+    public function plantCreate(Application $app, Request $request)
+    {
+        // some default data for when the form is displayed the first time
+        $data = array(
+            'name' => 'My first cactus',
+            'specie' => 'Cactus',
+        );
+
+        $form = $app['form.factory']->createBuilder('form', $data)
+            ->add('name')
+            ->add('specie')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+            $app['db']->insert('Plants', $data);
+            
+            //$app['db']
+
+
+            return $app->redirect('/plant');
+        }
+
+        // display the form
+        return $app['twig']->render('plantCreate.twig', array('form' => $form->createView()));
+        //return $app['twig']->render('plantCreate.twig', array('data' => print_r($app)));
+    }
+
+    public function plantDetail(Application $app, $id)
+    {
+        $plant = $app['db']->fetchAssoc('SELECT * FROM Plants WHERE id = ?', array($id));
+        return $app['twig']->render('plantDetail.twig', array('plant' => $plant));
+    }
 }
+
--- webapp/web/index.php
+++ webapp/web/index.php
@@ -11,9 +11,24 @@ $app['debug'] = true;
 
 // Services 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
+    'twig.path' => __DIR__.'/../res/views',
 ));
 
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+    	'dbname' => 'plantissime',
+    	'user' => 'admin',
+    	'password' => 'fXUQ|MjmNbds',
+    	'host' => 'db',
+    ),
+));
+
+$app->register(new Silex\Provider\FormServiceProvider());
+
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallbacks' => array('en'),
+));
 
 // Controllers
 $app->mount('/', new Plantissime\AppControllerProvider());
