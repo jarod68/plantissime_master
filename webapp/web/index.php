e95e75fbf8dfca508f649b04d0014597c4056d3e
Efynox <efynox@live.fr>
2015-02-24 00:20:30 +0100

    Add sensor list, sensor creation Add receive json request Move controllers into Controllers folder Rename somes classes

--- webapp/web/index.php
+++ webapp/web/index.php
@@ -4,36 +4,4 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Plantissime\PlantissimeApplication();
 
-
-// Config
-$app['debug'] = true;
-
-
-// Services 
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../res/views',
-));
-
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver'   => 'pdo_mysql',
-    	'dbname' => 'plantissime',
-    	'user' => 'admin',
-    	'password' => 'fXUQ|MjmNbds',
-    	'host' => 'db',
-    ),
-));
-
-$app->register(new Silex\Provider\FormServiceProvider());
-
-$app->register(new Silex\Provider\TranslationServiceProvider(), array(
-    'locale_fallbacks' => array('en'),
-));
-
-// Controllers
-$app->mount('/', new Plantissime\AppControllerProvider());
-$app->mount('/api', new Plantissime\ApiControllerProvider());
-
-
-
 $app->run();
