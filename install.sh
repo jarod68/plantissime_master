f11f3fe4c19039b998073404db0edbaec0fcc7c0
Efynox <efynox@live.fr>
2015-06-03 00:44:04 +0200

    implement scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #TODO
+
 # Install nodejs if not present
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+dpkg -i node_latest_armhf.deb
 
 # Install plantissime-api
 ## Install npm packages
