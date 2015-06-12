244a2cdc1ceb916da23d22da2b9ed2513d47ebba
Efynox <efynox@live.fr>
2015-06-12 17:59:01 +0200

    implement install script

--- install.sh
+++ install.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-#TODO
+# ONLY FOR RASPBIAN
 
 # Install nodejs if not present
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 dpkg -i node_latest_armhf.deb
 
+# Install gulp
+RUN npm install -g gulp
+
 # Install plantissime-api
+cd  plantissime-api
 ## Install npm packages
+npm install
+cd ..
 
 # Install plantissime-client
+#mkdir -p plantissime-client/semantic
+cd plantissime-client
 ## Install npm packages
+npm install
 ## Build semantic-ui 
+#TODO
+#RUN gulp build
+cd ..
+
+
+
