4fb2d66d11957cb5349277626bac43cb82b79996
Efynox <efynox@live.fr>
2015-06-13 14:28:41 +0200

    add installation of mysql, git and wget to install script

--- install.sh
+++ install.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 
 # ONLY FOR RASPBIAN
+apt-get update
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo "Checking installed dependencies :"
+
+# Install MySQL if not installed
+if [ `ls /usr/bin/mysql` != '/usr/bin/mysql' ]
+then
+	echo "${red} - MySQL not found${reset}"
+	echo "${red}   => Install MySQL...${reset}"
+	echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+	echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+	apt-get install -y mysql-server
+else
+	echo "${green} - MySQL already installed${reset}"
+fi
+
+# Install Git if not installed
+if [ `ls /usr/bin/git` != '/usr/bin/git' ]
+then
+	echo "${red} - Git not found${reset}"
+	echo "${red}   => Install Git...${reset}"
+	apt-get install -y git
+else
+	echo "${green} - Git already installed${reset}"
+fi
+
+# Install wget if not installed
+if [ `ls /usr/bin/wget` != '/usr/bin/wget' ]
+then
+	echo "${red} - wget not found${reset}"
+	echo "${red}   => Install wget...${reset}"
+	apt-get install -y git
+else
+	echo "${green} - wget already installed${reset}"
+fi
 
 # Install nodejs if not present
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 dpkg -i node_latest_armhf.deb
 
 # Install gulp
-RUN npm install -g gulp
+npm install -g gulp
 
 # Install plantissime-api
 cd  plantissime-api
