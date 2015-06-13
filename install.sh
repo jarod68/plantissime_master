1e4c3a6fa3f52458ddea37d63c74f5aeece514d0
Efynox <efynox@live.fr>
2015-06-13 19:11:58 +0200

    add gulp build to install script, add silent mode for apt-get in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # ONLY FOR RASPBIAN
-apt-get update
+echo "Updating system..."
+apt-get -qq update
 
 red=`tput setaf 1`
 green=`tput setaf 2`
@@ -10,40 +11,53 @@ reset=`tput sgr0`
 echo "Checking installed dependencies :"
 
 # Install MySQL if not installed
-if [ `ls /usr/bin/mysql` != '/usr/bin/mysql' ]
+if ls /usr/bin/mysql 2>/dev/null
 then
+	echo "${green} - MySQL already installed${reset}"
+else
 	echo "${red} - MySQL not found${reset}"
-	echo "${red}   => Install MySQL...${reset}"
+	echo "${red}   => Install MySQL . . .${reset}"
 	echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
 	echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
-	apt-get install -y mysql-server
-else
-	echo "${green} - MySQL already installed${reset}"
+	apt-get -qq -y install mysql-server
+	echo "${green}   => MySQL installed${reset}"
 fi
 
 # Install Git if not installed
-if [ `ls /usr/bin/git` != '/usr/bin/git' ]
+if ls /usr/bin/git 2>/dev/null
 then
-	echo "${red} - Git not found${reset}"
-	echo "${red}   => Install Git...${reset}"
-	apt-get install -y git
-else
 	echo "${green} - Git already installed${reset}"
+else
+	echo "${red} - Git not found${reset}"
+	echo "${red}   => Install Git . . .${reset}"
+	apt-get -qq -y install git
+	echo "${green}   => Git installed${reset}"
 fi
 
 # Install wget if not installed
-if [ `ls /usr/bin/wget` != '/usr/bin/wget' ]
+if ls /usr/bin/wget 2>/dev/null
 then
-	echo "${red} - wget not found${reset}"
-	echo "${red}   => Install wget...${reset}"
-	apt-get install -y git
-else
 	echo "${green} - wget already installed${reset}"
+else
+	echo "${red} - wget not found${reset}"
+	echo "${red}   => Install wget . . .${reset}"
+	apt-get -qq -y install wget
+	echo "${green}   => wget installed${reset}"
 fi
 
 # Install nodejs if not present
-wget http://node-arm.herokuapp.com/node_latest_armhf.deb
-dpkg -i node_latest_armhf.deb
+if ls /usr/local/bin/node
+then 
+	echo "${green} - node.js already installed${reset}"
+else
+	echo "${red} - node.js not found${reset}"
+	echo "${red}   => Install node.js . . .${reset}"
+
+	wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+	dpkg -i node_latest_armhf.deb
+
+	echo "${green}   => node.js installed${reset}"
+fi
 
 # Install gulp
 npm install -g gulp
@@ -60,8 +74,9 @@ cd plantissime-client
 ## Install npm packages
 npm install
 ## Build semantic-ui 
-#TODO
-#RUN gulp build
+cd semantic
+gulp build
+cd ..
 cd ..
 
 
