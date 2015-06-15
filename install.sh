6a38823c3a0c04b6c7402a106d2c3e8510003ebd
Efynox <efynox@live.fr>
2015-06-15 22:54:22 +0200

    move database creation from run script to install script

--- install.sh
+++ install.sh
@@ -1,83 +1,97 @@
 #!/bin/bash
 
 # ONLY FOR RASPBIAN
-echo "Updating system..."
+
+echo "Plantissime installation :"
+
+echo " - Updating system . . ."
 apt-get -qq update
 
 red=`tput setaf 1`
 green=`tput setaf 2`
+blue=`tput setaf 6`
 reset=`tput sgr0`
-
-echo "Checking installed dependencies :"
 
 # Install MySQL if not installed
 if ls /usr/bin/mysql 2>/dev/null
 then
-	echo "${green} - MySQL already installed${reset}"
+	echo " - MySQL : ${green}installed${reset}"
 else
-	echo "${red} - MySQL not found${reset}"
-	echo "${red}   => Install MySQL . . .${reset}"
+	echo " - MySQL : ${red}not found${reset}"
+	echo "${blue}   => Install MySQL . . .${reset}"
 	echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
 	echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
 	apt-get -qq -y install mysql-server
-	echo "${green}   => MySQL installed${reset}"
+	echo "${blue}   => MySQL installed${reset}"
 fi
 
 # Install Git if not installed
 if ls /usr/bin/git 2>/dev/null
 then
-	echo "${green} - Git already installed${reset}"
+	echo " - git : ${green}installed${reset}"
 else
-	echo "${red} - Git not found${reset}"
-	echo "${red}   => Install Git . . .${reset}"
+	echo " - git : ${red}not found${reset}"
+	echo "${blue}   => Install Git . . .${reset}"
 	apt-get -qq -y install git
-	echo "${green}   => Git installed${reset}"
+	echo "${blue}   => git installed${reset}"
 fi
 
 # Install wget if not installed
 if ls /usr/bin/wget 2>/dev/null
 then
-	echo "${green} - wget already installed${reset}"
+	echo " - wget : ${green}installed${reset}"
 else
-	echo "${red} - wget not found${reset}"
-	echo "${red}   => Install wget . . .${reset}"
+	echo " - wget : ${red}not found${reset}"
+	echo "${blue}   => Install wget . . .${reset}"
 	apt-get -qq -y install wget
-	echo "${green}   => wget installed${reset}"
+	echo "${blue}   => wget installed${reset}"
 fi
 
 # Install nodejs if not present
 if ls /usr/local/bin/node
-then 
-	echo "${green} - node.js already installed${reset}"
+then
+	echo " - node.js : ${green}installed${reset}"
 else
-	echo "${red} - node.js not found${reset}"
-	echo "${red}   => Install node.js . . .${reset}"
+	echo " - node.js : ${red}not found${reset}"
+	echo "${blue}   => Install node.js . . .${reset}"
 
 	wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 	dpkg -i node_latest_armhf.deb
 
-	echo "${green}   => node.js installed${reset}"
+	echo "${blue}   => node.js installed${reset}"
 fi
 
 # Install gulp
+echo " - gulp : ${blue}installation . . .${reset}"
 npm install -g gulp
+echo "${blue}   => gulp installed${reset}"
+
 
 # Install plantissime-api
+echo " - plantissime-api : ${blue}installation . . .${reset}"
 cd  plantissime-api
-## Install npm packages
 npm install
 cd ..
+echo "${blue}   => plantissime-api installed${reset}"
+
 
 # Install plantissime-client
-#mkdir -p plantissime-client/semantic
+echo " - plantissime-client : ${blue}installation . . .${reset}"
 cd plantissime-client
-## Install npm packages
 npm install
-## Build semantic-ui 
+echo "${blue}   => plantissime-client installed${reset}"
+
+
+# Build semantic-ui 
+echo " - plantissime-client : ${blue}building . . .${reset}"
 cd semantic
 gulp build
+echo "${blue}   => plantissime-client built${reset}"
 cd ..
 cd ..
 
-
-
+# MySQL
+echo " - MySQL database : ${blue}creation . . .${reset}"
+service mysql start
+mysql --user=root --password=root -e "CREATE DATABASE plantissime;"
+service mysql stop
