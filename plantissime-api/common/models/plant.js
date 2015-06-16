08f88b65e0d5c43c685d30b15545e54ee08053ba
Efynox <efynox@live.fr>
2015-06-16 23:47:51 +0200

    delegate add measure to target instead of sensor

--- plantissime-api/common/models/plant.js
+++ plantissime-api/common/models/plant.js
@@ -1,4 +1,21 @@
 module.exports = function(Plant) {
 
+  Plant.prototype.addMeasure = function(value, measureType) {
+    // Measure creation
+    var measureToAdd = { 
+      value: value,
+      type: measureType, 
+      time: new Date(), 
+      targetId: this.id
+    };
+
+
+    
+    Plant.app.models.Measure.create(measureToAdd, function(err, data) {
+      console.log('-- Measure created --');
+      console.log(data);
+      console.log('---------------------');
+    });
+  };
 	
 };
