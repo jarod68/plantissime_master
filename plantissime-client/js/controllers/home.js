5245110ca68689c8d16b13443c7ab8ebcf485964
Efynox <efynox@live.fr>
2015-07-02 21:57:02 +0200

    add date and time to home

--- plantissime-client/js/controllers/home.js
+++ plantissime-client/js/controllers/home.js
@@ -1,8 +1,12 @@
-planti.controllers.controller('HomeController', function ($scope, $http) {
+planti.controllers.controller('HomeController', function ($scope, $http, $interval) {
   $scope.section = "home";
   $scope.home = true;
   
   $scope.currentDate = new Date();
+  
+  $interval(function() {
+    $scope.currentDate = new Date();
+  }, 1000);
   
   $http.get('/api/events?filter[where][level]=1&filter[order]=time DESC&filter[limit]=10&filter[include]=target')
     .success(function(data) {
