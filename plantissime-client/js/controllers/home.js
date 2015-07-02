655c2ba0057ca3b803ad4776a4482eee60e685a4
Efynox <efynox@live.fr>
2015-07-02 21:18:54 +0200

    add events and alerts list on home

--- plantissime-client/js/controllers/home.js
+++ plantissime-client/js/controllers/home.js
@@ -1,4 +1,16 @@
-planti.controllers.controller('HomeController', function ($scope) {
+planti.controllers.controller('HomeController', function ($scope, $http) {
   $scope.section = "home";
   $scope.home = true;
+  
+  $scope.currentDate = new Date();
+  
+  $http.get('/api/events?filter[where][level]=1&filter[order]=time DESC&filter[limit]=10&filter[include]=target')
+    .success(function(data) {
+      $scope.events = data;
+      console.log(data);
+  });  
+  $http.get('/api/events?filter[where][and][0][level]=2&filter[where][and][1][expiredAt]=null&filter[order]=time DESC&filter[limit]=10&filter[include]=target')
+    .success(function(data) {
+      $scope.alerts = data;
+  });
 });
