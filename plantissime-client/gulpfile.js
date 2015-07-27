5b94580a970bb7947373d8e6089c8c3c8a48a305
Efynox <efynox@live.fr>
2015-07-27 21:56:30 +0200

    add gulp build scripts task

---
+++ plantissime-client/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+
+var paths = {
+  scripts: ['js/**/*.js'],
+};
+
+gulp.task('clean', function(cb) {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  del(['build'], cb);
+});
+
+gulp.task('scripts', ['clean'], function() {
+  return gulp.src(paths.scripts)
+    //.pipe(uglify())
+    .pipe(concat('plantissime.min.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['scripts']);
