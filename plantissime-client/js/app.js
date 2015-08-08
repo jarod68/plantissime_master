a3880fd0281b749648749a76e832da43c00b3c1b
Efynox <efynox@live.fr>
2015-08-08 16:03:16 +0200

    switch from angular-route to angular-ui-router

--- plantissime-client/js/app.js
+++ plantissime-client/js/app.js
@@ -5,8 +5,8 @@
  *  Description : 
  *  Year        :  2015
  */
-var app = angular.module('PlantissimeApp', [
-	'ngRoute',
+var plantiApp = angular.module('PlantissimeApp', [
+	'ui.router',
 	'planti.controllers',
 	'planti.directives',
   'planti.services',
@@ -20,32 +20,3 @@ planti.controllers = angular.module('planti.controllers', ["chart.js"]);
 planti.directives = angular.module('planti.directives', []);
 planti.services = angular.module('planti.services', ['ngResource']);
 planti.filters = angular.module('planti.filters', []);
-
-// Routing
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/home', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeController'
-      }).
-      when('/plants', {
-        templateUrl: 'views/plant-list.html',
-        controller: 'PlantsController'
-      }).
-      when('/plants/:plantId', {
-        templateUrl: 'views/plant-detail.html',
-        controller: 'PlantDetailController'
-      }).
-      when('/sensors', {
-        templateUrl: 'views/sensor-list.html',
-        controller: 'SensorsController'
-      })
-      .when('/settings', {
-        templateUrl: 'views/settings.html',
-        controller: 'SettingsController'
-      }).
-      otherwise({
-        redirectTo: '/home'
-      });
-  }]);
